755a68d443c443acc9437bdddeaeadeeaa1366c5
Murat Kasimov <iokasimov.m@gmail.com>
2023-01-29 20:50:38 +0700

    Create rules.mk

---
+++ rules.mk
@@ -0,0 +1,11 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+ORYX_ENABLE = yes
+SPACE_CADET_ENABLE = no
+UNICODE_ENABLE = yes
+# COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+
+SRC = matrix.c
